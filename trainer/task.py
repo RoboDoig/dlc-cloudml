8d9592b993d914734e0fab4243c17573294e7712
Andrew Erskine <erskine94@gmail.com>
2018-09-03 15:38:48 -0700

    initial attempt at cloudml port

---
+++ trainer/task.py
@@ -0,0 +1,49 @@
+import tensorflow as tf
+import argparse
+import os
+import sys
+
+from trainer import train
+
+FLAGS = None
+
+
+def main(_):
+    train.train(FLAGS)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--data_dir',
+        help='Directory to find training data',
+        default='../test_data/'
+    )
+    parser.add_argument(
+        '--task',
+        help='Task name',
+        default='reaching'
+    )
+    parser.add_argument(
+        '--date',
+        help='Data date',
+        default='Jan30'
+    )
+    parser.add_argument(
+        '--train_fraction',
+        help='Train fraction',
+        default=0.95
+    )
+    parser.add_argument(
+        '--shuffle',
+        help='Shuffle',
+        default=1
+    )
+    parser.add_argument(
+        '--weights_dir',
+        help='Directory to find inital weights',
+        default='../models/'
+    )
+
+    FLAGS, unparsed = parser.parse_known_args()
+    tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
